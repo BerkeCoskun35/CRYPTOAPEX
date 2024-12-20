9e552ac00a7eab4bfdf906e06a0d959c0b17bcde
Soner1179 <sonereski1179@gmail.com>
2024-12-20 18:36:57 +0300

    20.12.24 18.36
    dövizler güncellendi

--- api.py
+++ api.py
@@ -1,129 +1,114 @@
-import requests
-import time
-from database import get_db_connection  # Veritabanı bağlantısı
+from flask import Flask, jsonify, render_template, request, session
+import hashlib
+from database import get_db_connection
+from api import start_price_updater
+import threading
 
-# ExchangeRate API anahtarı
-EXCHANGERATE_API_KEY = "ae3a1b4145458a01849153bf"
+app = Flask(__name__)
+app.secret_key = 'cryptoapex_x'
 
-def get_all_crypto_prices():
-    """
-    Binance API'den kripto para fiyatlarını çeker.
-    """
-    url = "https://api.binance.com/api/v3/ticker/price"
-    response = requests.get(url)
-    if response.status_code != 200:
-        print("Binance API isteği başarısız oldu:", response.status_code, response.text)
-        return {}
-    
-    data = response.json()
-    return {item["symbol"]: float(item["price"]) for item in data}
+def hash_password(password):
+    sha256_hash = hashlib.sha256()
+    sha256_hash.update(password.encode('utf-8'))
+    return sha256_hash.hexdigest()
 
-def get_all_currency_rates():
-    """
-    ExchangeRate API'den döviz fiyatlarını çeker.
-    """
-    url = f"https://v6.exchangerate-api.com/v6/{EXCHANGERATE_API_KEY}/latest/TRY"
-    response = requests.get(url)
-    if response.status_code != 200:
-        print("ExchangeRate API isteği başarısız oldu:", response.status_code, response.text)
-        return {}
-    
-    data = response.json()
-    rates = data.get("conversion_rates", {})
-    return rates
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    message = None
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+        hashed_password = hash_password(password)
 
-def update_crypto_prices():
-    """
-    Veritabanındaki kripto para fiyatlarını Binance API'den güncelleyerek günceller.
-    """
-    all_prices = get_all_crypto_prices()
-    if not all_prices:
-        print("Kripto fiyatları alınamadı.")
-        return
-    
+        db = get_db_connection()
+        users = db['users']
+        try:
+            users.insert_one({
+                'username': username,
+                'email': email,
+                'password': hashed_password
+            })
+            message = 'Kayıt başarılı! Artık giriş yapabilirsiniz.'
+        except Exception as e:
+            message = 'Bu kullanıcı adı veya e-postası zaten kayıtlı.'
+
+    return render_template('register.html', message=message)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    message = None
+    if request.method == 'POST':
+        username_or_email = request.form.get('username-email')
+        password = request.form.get('password')
+        hs_password = hash_password(password)
+
+        # MongoDB bağlantısı
+        db = get_db_connection()
+        users = db['users']
+        
+        # Kullanıcıyı sorgula
+        user = users.find_one({'$or': [{'username': username_or_email}, {'email': username_or_email}]})
+        
+        # Kullanıcı doğrulama
+        if user:
+            if user.get('password') == hs_password:
+                session['user_id'] = str(user['_id'])
+                session['username'] = user['username']
+                message = 'Giriş başarılı!'
+            else:
+                message = 'Şifre hatalı.'
+        else:
+            message = 'Kullanıcı adı veya e-posta bulunamadı.'
+
+        if message == 'Giriş başarılı!':
+            updated_prices = get_updated_prices()
+            return render_template('index.html', message=message, updated_prices=updated_prices)
+
+    return render_template('login.html', message=message)
+
+
+def get_updated_prices():
     db = get_db_connection()
     crypto_collection = db['Kripto Para']
-
-    coins = list(crypto_collection.find())
-    for coin in coins:
-        coin_symbol = coin["kripto_adi"] + "USDT"
-        if coin_symbol in all_prices:
-            current_price = all_prices[coin_symbol]
-            current_time = time.strftime('%Y-%m-%d %H:%M:%S')
-
-            crypto_collection.update_one(
-                {'_id': coin['_id']},
-                {'$set': {'guncel_fiyat': current_price, 'guncellenme_zamani': current_time}}
-            )
-            print(f"{coin_symbol} fiyatı güncellendi: {current_price}")
-
-def update_currency_prices():
-    """
-    Veritabanındaki döviz fiyatlarını ExchangeRate API'den güncelleyerek günceller.
-    """
-    all_rates = get_all_currency_rates()
-    if not all_rates:
-        print("Döviz kurları alınamadı.")
-        return
-    
-    db = get_db_connection()
     currency_collection = db['Döviz']
 
-    currencies = list(currency_collection.find())
-    for currency in currencies:
-        currency_name = currency["döviz_adi"]
-        if currency_name in all_rates:
-            current_price_in_try = all_rates[currency_name]
-            current_time = time.strftime('%Y-%m-%d %H:%M:%S')
+    # Kripto ve döviz fiyatlarını alın
+    crypto_prices = list(crypto_collection.find())
+    currency_prices = list(currency_collection.find())
 
-            currency_collection.update_one(
-                {'_id': currency['_id']},
-                {'$set': {'guncel_fiyat': current_price_in_try, 'guncellenme_zamani': current_time}}
-            )
-            print(f"{currency_name} fiyatı (1 birim kaç TL): {current_price_in_try}")
+    # Kripto fiyatlarını düzenle
+    updated_crypto_prices = {
+        price['kripto_adi']: f"{price['guncel_fiyat']}" for price in crypto_prices
+    }
+    # Döviz fiyatlarını düzenle
+    updated_currency_prices = {
+        price['döviz_adi']: f"{price['guncel_fiyat']}" for price in currency_prices
+    }
 
-def save_prices_to_history():
-    """
-    Kripto ve döviz fiyatlarını geçmiş koleksiyonlara kaydeder.
-    """
-    db = get_db_connection()
-    crypto_collection = db['Kripto Para']
-    crypto_history = db['Kripto_Gecmis']
+    # İki fiyat listesini birleştir
+    updated_prices = {**updated_crypto_prices, **updated_currency_prices}
+    return updated_prices
 
-    for coin in crypto_collection.find():
-        crypto_history.insert_one({
-            'kripto_id': coin['_id'],
-            'fiyat': coin['guncel_fiyat'],
-            'tarih': time.strftime('%Y-%m-%d %H:%M:%S')
-        })
+@app.route('/')
+def index():
+    updated_prices = get_updated_prices()
+    return render_template('index.html', updated_prices=updated_prices)
 
-    currency_collection = db['Döviz']
-    currency_history = db['Döviz_Gecmis']
+@app.route('/api/prices', methods=['GET'])
+def get_prices():
+    updated_prices = get_updated_prices()
+    return jsonify(updated_prices)
 
-    for currency in currency_collection.find():
-        currency_history.insert_one({
-            'döviz_id': currency['_id'],
-            'fiyat': currency['guncel_fiyat'],
-            'tarih': time.strftime('%Y-%m-%d %H:%M:%S')
-        })
+@app.route('/logout')
+def logout():
+    session.pop('user_id', None)
+    session.pop('username', None)
+    message = 'Çıkış yapıldı.'
+    return render_template('login.html', message=message)
 
-    print("Fiyatlar geçmiş tablolara kaydedildi.")
+price_updater_thread = threading.Thread(target=start_price_updater, daemon=True)
+price_updater_thread.start()
 
-def start_price_updater():
-    """
-    Kripto ve döviz fiyatlarını düzenli olarak günceller ve geçmiş kaydeder.
-    """
-    update_count = 0
-    while True:
-        update_crypto_prices()
-        update_currency_prices()
-        update_count += 1
-
-        if update_count % 2 == 0:
-            save_prices_to_history()
-
-        time.sleep(30)  # 30 saniyede bir fiyat güncellemesi
-
-# Kullanım
-if __name__ == "__main__":
-    start_price_updater()
+if __name__ == '__main__':
+    app.run(debug=False)
